0db73fc055e68ab094ea207bed152b3d6f5098ce
wy_xue <xueweiyu@live.cn>
2018-06-08 10:53:38 +0800

    MVC

---
+++ src/DesignPattern/MVCPattern/Student.java
@@ -0,0 +1,25 @@
+package DesignPattern.MVCPattern;
+
+/**
+ * Created by wy_xue on 2018/6/8.
+ */
+public class Student {
+    private String rollNo;
+    private String name;
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
